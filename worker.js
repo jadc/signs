a632b2fb2025bce05ebd220e20b8c9fe8f149100
jadc <chehimj@gmail.com>
2023-08-29 20:34:09 -0600

    fixed icon, made external

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ self.addEventListener("install", function(event) {
     event.waitUntil(
         caches.open("signs")
             .then(function(cache) {
-                return cache.addAll(["index.html", "style.css", "script.js"]);
+                return cache.addAll(["index.html", "style.css", "script.js", "trash.svg"]);
             })
     );
 });
